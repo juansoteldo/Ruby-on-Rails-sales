7118c4a891b6f8557db06c858e6710aea3a0f11f
juansoteldo <jsoteldoup@gmail.com>
2016-04-30 20:27:45 -0400

    add Streak stuff

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -4,14 +4,14 @@ class OrdersCreateJob < ActiveJob::Base
   	Streak.api_key = ENV['STREAK_API_KEY']
     order = ShopifyAPI::Order.new(params[:webhook])
 		email = order.customer.email
-		is_deposit? = order.line_items.any?{|line_item| line_item.title.include? "Deposit" }
-		is_final? = order.line_items.any?{|line_item| line_item.title.include? "Final" }
+		is_deposit = order.line_items.any?{|line_item| line_item.title.include? "Deposit" }
+		is_final = order.line_items.any?{|line_item| line_item.title.include? "Final" }
 		order.note_attributes.each do |note_attr|
 			if note_attr.name == "req_id"
 				@req_id = note_attr.value
 			end
 		end
-		if is_deposit?
+		if is_deposit
 			quoted_stage = ENV['QUOTE_STAGE']
 			deposited_stage = ENV['DEPOSIT_STAGE']
 			boxes = Streak::Box.all(ENV['STREAK_PIPELINE_ID']).select { |b| b.email_addresses.include? email and b.stage_key == quoted_stage and b.total_number_of_sent_emails >= 1 }
@@ -23,7 +23,7 @@ class OrdersCreateJob < ActiveJob::Base
       	request.update_attribute(:deposit_order_id, order.id)
       end
 		end
-		if is_final?
+		if is_final
       if @req_id
       	request = Request.find(@req_id)
       	request.update_attribute(:final_order_id, order.id)
