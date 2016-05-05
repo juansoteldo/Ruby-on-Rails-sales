4d28d8c8e45b795ecf3e3be4b4f03dd8f7b71602
juansoteldo <jsoteldoup@gmail.com>
2016-05-04 22:06:18 -0400

    fix public controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,6 @@ class ApplicationController < ActionController::Base
     
     headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
     headers['Access-Control-Allow-Credentials'] = 'true'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
   def options
