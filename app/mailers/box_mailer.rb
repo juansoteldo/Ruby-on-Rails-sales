3cc42e5a7857be043df79acb1843ea96b1f9c117
juansoteldo <jsoteldoup@gmail.com>
2016-06-13 13:40:51 -0400

    add final confirmation email

--- app/mailers/box_mailer.rb
+++ app/mailers/box_mailer.rb
@@ -11,8 +11,10 @@ class BoxMailer < ActionMailer::Base
 
   def final_confirmation_email(email)
   	@user = User.find_by_email(email)
-  	@request = @user.requests.first
-    mail(to: email, bcc: 'sales@customtattoodesign.ca, KaylaMckee@customtattoodesign.ca', subject: 'Custom Tattoo Design - Order Confirmation')
+  	if @user
+		@request = @user.requests.first
+		mail(to: email, bcc: 'sales@customtattoodesign.ca, KaylaMckee@customtattoodesign.ca', subject: 'Custom Tattoo Design - Thank You')
+	end
   end
 
 end
