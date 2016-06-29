5c74564a7a15432306c5ede595a784ca419a527a
juansoteldo <jsoteldoup@gmail.com>
2016-06-29 12:14:13 -0400

    Removed bccs from mailer

--- app/mailers/box_mailer.rb
+++ app/mailers/box_mailer.rb
@@ -2,11 +2,17 @@ class BoxMailer < ActionMailer::Base
 	default from: 'orders@customtattoodesign.ca'
  
   def reminder_email(email)
-    mail(to: email, bcc: 'sales@customtattoodesign.ca, KaylaMckee@customtattoodesign.ca', subject: 'Lee Roller Owner / Custom Tattoo Design', reply_to: 'leeroller@customtattoodesign.ca', display_name: 'Lee Roller')
+    mail(to: email,
+         bcc: Rails.application.config.action_mailer_bcc,
+         subject: 'Lee Roller Owner / Custom Tattoo Design',
+         reply_to: 'leeroller@customtattoodesign.ca',
+         display_name: 'Lee Roller')
   end
 
   def confirmation_email(email)
-    mail(to: email, bcc: 'sales@customtattoodesign.ca, KaylaMckee@customtattoodesign.ca', subject: 'Custom Tattoo Design - Order Confirmation')
+    mail(to: email,
+         bcc: Rails.application.config.action_mailer_bcc,
+         subject: 'Custom Tattoo Design - Order Confirmation')
   end
 
   def final_confirmation_email(email)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer_bcc = []
 end
