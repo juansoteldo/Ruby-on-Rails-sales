0bf4229dd5e151350c23e417a2b8afe0fd7c93cf
juansoteldo <jsoteldoup@gmail.com>
2016-07-11 13:57:54 -0400

    Patch improvements

--- app/models/streak_api/pipeline.rb
+++ app/models/streak_api/pipeline.rb
@@ -1,19 +1,23 @@
 require 'streak_api/base'
 
 class StreakAPI::Pipeline < StreakAPI::Base
+
 	def self.all
-		Streak.api_key = Rails.application.config.streak_api_key
-		Rails.cache.fetch('streak_pipline/all', expires_in: 1.minutes) do
+		Rails.cache.fetch('streak_pipline/all', expires_in: 15.minutes) do
+			Streak.api_key = Rails.application.config.streak_api_key
 			Streak::Pipeline.all()
 		end
 	end
-	def self.find_by_name pipeline_name
-		Streak.api_key = Rails.application.config.streak_api_key
-		Rails.cache.fetch('streak_pipline/find', expires_in: 1.minutes) do
-			Streak::Pipeline.all().select{|p| p.name == pipeline_name}.first
-		end
+
+	def self.default
+		StreakAPI::Pipeline.all.select{
+				|p| p.name == 'CTD Sales'
+		}.first
 	end
+
 	def self.user_keys
-		StreakAPI::Pipeline.find_by_name("CTD Sales").acl_entries.map{|u| u.user_key}
+		StreakAPI::Pipeline.default.acl_entries.map { |u|
+			u.user_key
+		}
 	end
 end
--- app/models/streak_api/user.rb
+++ app/models/streak_api/user.rb
@@ -2,12 +2,15 @@ require 'streak_api/base'
 
 class StreakAPI::User < StreakAPI::Base
 	def self.find(key)
-		Streak.api_key = Rails.application.config.streak_api_key
-		Streak::User.find(key)
+		Rails.cache.fetch( 'streak_user/'+key, expires_in: 15.minutes ) do
+			Streak.api_key = Rails.application.config.streak_api_key
+			Streak::User.find(key)
+		end
 	end
-	def self.find_key_by_email(email)
+
+	def self.find_by_email(email)
 		StreakAPI::Pipeline.user_keys.select{ |k|
-			user = Streak::User.find(k)
+			user = StreakAPI::User.find(k)
 			user.email == email
 		}.first
 	end
