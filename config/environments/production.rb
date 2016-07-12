c76873de3a18b714c92178ac57596e732d7cf97f
juansoteldo <jsoteldoup@gmail.com>
2016-07-12 12:55:35 -0400

    Added back sales to bcc list.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer_bcc = []
+  config.action_mailer_bcc = [ 'sales@customtattoodesign.ca' ]
 end
