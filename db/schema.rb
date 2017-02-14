c80195c6ae8aa4a5044d79a9f29cf7e12e9295f5
juansoteldo <jsoteldoup@gmail.com>
2017-02-14 10:27:25 -0500

    More detailed schmea

--- db/schema.rb
+++ db/schema.rb
@@ -17,35 +17,35 @@ ActiveRecord::Schema.define(version: 20170214141057) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at",    precision: 6
+    t.integer  "sign_in_count",                        default: 0,  null: false
+    t.datetime "current_sign_in_at",     precision: 6
+    t.datetime "last_sign_in_at",        precision: 6
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",             precision: 6,              null: false
+    t.datetime "updated_at",             precision: 6,              null: false
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
+    t.integer  "priority",                 default: 0, null: false
+    t.integer  "attempts",                 default: 0, null: false
+    t.text     "handler",                              null: false
     t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
+    t.datetime "run_at",     precision: 6
+    t.datetime "locked_at",  precision: 6
+    t.datetime "failed_at",  precision: 6
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
@@ -53,9 +53,9 @@ ActiveRecord::Schema.define(version: 20170214141057) do
   create_table "delivered_emails", force: :cascade do |t|
     t.integer  "request_id"
     t.integer  "marketing_email_id"
-    t.datetime "sent_at"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "sent_at",            precision: 6
+    t.datetime "created_at",         precision: 6, null: false
+    t.datetime "updated_at",         precision: 6, null: false
   end
 
   add_index "delivered_emails", ["marketing_email_id"], name: "index_delivered_emails_on_marketing_email_id", using: :btree
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20170214141057) do
 
   create_table "requests", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "first_name",       default: ""
-    t.string   "last_name",        default: ""
+    t.string   "first_name",                     default: ""
+    t.string   "last_name",                      default: ""
     t.string   "token"
     t.boolean  "is_first_time"
     t.string   "gender"
@@ -92,17 +92,17 @@ ActiveRecord::Schema.define(version: 20170214141057) do
     t.string   "request_id"
     t.string   "client_id"
     t.string   "ticket_id"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at",       precision: 6,                   null: false
+    t.datetime "updated_at",       precision: 6,                   null: false
     t.string   "_ga"
     t.string   "linker_param"
     t.string   "handle"
     t.string   "variant"
-    t.datetime "last_visited_at"
+    t.datetime "last_visited_at",  precision: 6
     t.string   "deposit_variant"
     t.integer  "quoted_by_id"
-    t.string   "state",            default: "fresh"
-    t.datetime "state_changed_at"
+    t.string   "state",                          default: "fresh"
+    t.datetime "state_changed_at", precision: 6
   end
 
   add_index "requests", ["quoted_by_id"], name: "index_requests_on_quoted_by_id", using: :btree
@@ -110,18 +110,18 @@ ActiveRecord::Schema.define(version: 20170214141057) do
   add_index "requests", ["user_id"], name: "index_requests_on_user_id", using: :btree
 
   create_table "salespeople", force: :cascade do |t|
-    t.string   "email",                    default: "", null: false
-    t.string   "encrypted_password",       default: "", null: false
+    t.string   "email",                                  default: "", null: false
+    t.string   "encrypted_password",                     default: "", null: false
     t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",            default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "reset_password_sent_at",   precision: 6
+    t.datetime "remember_created_at",      precision: 6
+    t.integer  "sign_in_count",                          default: 0,  null: false
+    t.datetime "current_sign_in_at",       precision: 6
+    t.datetime "last_sign_in_at",          precision: 6
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",               precision: 6,              null: false
+    t.datetime "updated_at",               precision: 6,              null: false
     t.string   "encrypted_streak_api_key"
     t.string   "user_key"
     t.boolean  "is_active"
@@ -132,20 +132,20 @@ ActiveRecord::Schema.define(version: 20170214141057) do
   add_index "salespeople", ["reset_password_token"], name: "index_salespeople_on_reset_password_token", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                             default: "",    null: false
-    t.string   "encrypted_password",                default: "",    null: false
+    t.string   "email",                                           default: "",    null: false
+    t.string   "encrypted_password",                              default: "",    null: false
     t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "reset_password_sent_at",            precision: 6
+    t.datetime "remember_created_at",               precision: 6
+    t.integer  "sign_in_count",                                   default: 0,     null: false
+    t.datetime "current_sign_in_at",                precision: 6
+    t.datetime "last_sign_in_at",                   precision: 6
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "shopify_id",             limit: 8
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.boolean  "opted_out",                         default: false
+    t.datetime "created_at",                        precision: 6,                 null: false
+    t.datetime "updated_at",                        precision: 6,                 null: false
+    t.boolean  "opted_out",                                       default: false
     t.string   "authentication_token",   limit: 30
   end
 
@@ -154,4 +154,6 @@ ActiveRecord::Schema.define(version: 20170214141057) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["shopify_id"], name: "index_users_on_shopify_id", using: :btree
 
+  add_foreign_key "delivered_emails", "requests"
+  add_foreign_key "requests", "users"
 end
