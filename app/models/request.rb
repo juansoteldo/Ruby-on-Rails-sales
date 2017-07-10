12f9defe46a85cb6fdfde1214680e2ff05201583
juansoteldo <jsoteldoup@gmail.com>
2017-07-10 18:15:29 -0400

    deleting image files for sure

--- app/models/request.rb
+++ app/models/request.rb
@@ -70,10 +70,12 @@ class Request < ActiveRecord::Base
     return if file.empty?
     begin
       return unless File.exists?(file)
-      return if images.where("file LIKE ?", "%#{File.basename(file)}%").any?
+      if images.where("file LIKE ?", "%#{File.basename(file)}%").any?
+        File.unlink file
+        return
+      end
       logger.info "Adding #{file}"
       images.create file: File.new(file)
-      File.unlink file
     rescue => e
       logger.error ">>> Cannot add image to request #{file}"
       logger.error e.message
@@ -82,7 +84,7 @@ class Request < ActiveRecord::Base
       begin
         File.unlink file if File.exists?(file)
       rescue
-
+        logger.error "Cannot delete image file at #{file}"
       end
     end
   end
