f54eeddab6f5667211251d76786fe4e094308c19
juansoteldo <jsoteldoup@gmail.com>
2017-07-19 09:38:23 -0400

    fixing problems with requestcreatejob

--- app/jobs/request_create_job.rb
+++ app/jobs/request_create_job.rb
@@ -2,18 +2,16 @@ class RequestCreateJob < ActiveJob::Base
   queue_as :webhook
 
   def perform(params)
-    @params = params.compact_empties
+    @params = params.compact
 
     set_user_by_email
-    @params.delete :email
-
     make_request!
   end
 
   def make_request!
-
-    @request = Request.recent.where(user_id: @user.id, position: params[:position]).first_or_create
-    @request.update! @params.compact
+    @request = Request.recent.where(user_id: @user.id,
+                                    position: @params[:position]).first_or_create
+    @request.assign_attributes @params
     @request.save!
 
     @user.requests << @request
@@ -21,24 +19,23 @@ class RequestCreateJob < ActiveJob::Base
 
   private
 
-  def params
-    @params
-  end
-
   def set_user_by_email
-    normalize_email
-    raise "empty email" if params[:email].nil? || params[:email].empty?
+    normalize_email!
+    raise "empty email" if @params[:email].nil? || @params[:email].empty?
 
-    if User.where(email: params[:email]).any?
-      @user =  User.find_by_email params.delete(:email)
+    if User.where(email: @params[:email]).any?
+      @user =  User.find_by_email @params.delete(:email)
     else
       password = SecureRandom.hex(8)
-      @user = User.create email: params.delete(:email), password: password, password_confirmation: password
+      @user = User.create email: @params.delete(:email), password: password, password_confirmation: password
     end
   end
 
-  def normalized_email
-    params[:email].downcase.strip
+  def normalize_email!
+    raise "empty email" if @params[:email].nil?
+    @params[:email] = @params[:email].downcase.strip
+    raise "empty email" if @params[:email] == ""
+    @params[:email]
   end
 end
 
