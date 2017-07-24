e9e6f09f2b7b2ad152d348e45c609ac7ff2cb029
juansoteldo <jsoteldoup@gmail.com>
2017-07-23 23:35:22 -0400

    working checkout

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -5,41 +5,36 @@ class PublicController < ApplicationController
 
   before_action :normalize_email
   before_action :validate_parameters, only: [:new_request]
-  before_action :set_user_by_email, only: [:new_request, :get_links]
-  before_action :set_salesperson_by_email, only: [ :get_links ]
-  before_action :load_products, only: [ :get_links ]
-  before_filter :set_user_by_client_id, only: [ :get_uid ]
-  before_filter :set_request_by_email, only: [ :get_ids, :get_links ]
+  before_action :set_user_by_email, only: %i(new_request get_links)
+  before_action :set_salesperson_by_email, only: [:get_links]
+  before_action :load_products, only: [:get_links]
+  before_action :set_user_by_client_id, only: [:get_uid]
+  before_action :set_shopify_order, only: [:deposit_redirect]
+  before_action :set_request_by_id, only: [:deposit_redirect]
+  before_action :set_request_by_email, only: [:get_ids, :get_links, :deposit_redirect]
 
 
   def redirect
-    if params[:requestId] != nil && Request.where(id: params[:requestId] ).any?
-      @request = Request.find(params[:requestId])
-    end
-    if not @request and ( params[:_ga] and Request.where(_ga: params[:_ga]).any? )
-      @request = Request.find_by__ga(params[:_ga])
-    end
+    @request = Request.find(params[:requestId]) if params[:requestId].present? && Request.find(params[:requestId])
+    @request ||= Request.find_by__ga(params[:_ga]) if params[:_ga] && Request.where(_ga: params[:_ga]).any?
 
     @url = 'http://shop.customtattoodesign.ca/products/'
-
-    if @request and @request.client_id and @request.client_id != 'false'
-      @request.update_columns( variant: params[:variant], handle: params[:handle], last_visited_at: Time.now )
+    if @request&.client_id && @request.client_id != ('false')
+      @request.update_columns(variant: params[:variant], handle: params[:handle], last_visited_at: Time.now)
       @url += "#{params[:handle]}?variant=#{params[:variant]}&uid=#{@request.user_id}&cid=#{@request.client_id}&reqid=#{@request.id}"
     elsif @request
       @url += "#{params[:handle]}?variant=#{params[:variant]}&utm_campaign=blocked&utm_source=crm&utm_medium=email&reqid=#{@request.id}"
     else
       @url += "#{params[:handle]}?variant=#{params[:variant]}&utm_campaign=unlisted&utm_source=crm&utm_medium=email"
     end
-    if params[:salesId]
-      @url += "&salesid=#{params[:salesId]}"
-    end
+    @url += "&salesid=#{params[:salesId]}" if params[:salesId]
   end
 
   def new_request
     request_params
     if Request.recent.where(user_id: @user.id, position: params[:position]).any?
       @request = Request.recent.where(user_id: @user.id, position: params[:position]).first
-      @request.update(request_params.reject{|_, v| v.blank?})
+      @request.update(request_params.reject { |_, v| v.blank? })
     else
       @request = Request.create(request_params)
       @request.save
@@ -47,17 +42,28 @@ class PublicController < ApplicationController
     end
   end
 
+  def deposit_redirect
+    @order.update_request
+
+    if @request
+      @crm_url = ENV.fetch "CRM_URL", "https://crm.customtattoodesign.ca"
+      @crm_url += "/preview/jobs/new?request_id=" + @request.id.to_s
+      respond_to do |format|
+        format.json { render json: (!@request.deposit_order_id.nil?)}
+        format.html {}
+      end
+    else
+      render text: "Request not found"
+    end
+  end
+
   def get_uid
     render json: @user.id
   end
 
-  def get_links
+  def get_links; end
 
-  end
-
-  def get_ids
-
-  end
+  def get_ids; end
 
   def set_link
     @request = Request.find(params[:request_id])
@@ -65,11 +71,11 @@ class PublicController < ApplicationController
     @request.quoted_by_id = params[:salesperson_id]
     @request.save!
 
-    box = StreakAPI::Box.find_by_email( params[:email] )
+    box = StreakAPI::Box.find_by_email(params[:email])
     if box
       current_stage = StreakAPI::Stage.find(key: box.stage_key)
-      if current_stage.name == "Contacted" || current_stage.name == "Leads"
-        StreakAPI::Box.set_stage(box.key, "Quoted")
+      if current_stage.name == 'Contacted' || current_stage.name == 'Leads'
+        StreakAPI::Box.set_stage(box.key, 'Quoted')
       end
     end
     head :ok
@@ -78,17 +84,17 @@ class PublicController < ApplicationController
   def save_email
     from_email = params[:from_email].downcase.strip
     recipient_email = params[:recipient_email]
-    @salesperson = Salesperson.find_by_email( from_email )
+    @salesperson = Salesperson.find_by_email(from_email)
     box = StreakAPI::Box.find_by_email(recipient_email)
     if box
       current_stage = StreakAPI::Stage.find(key: box.stage_key)
 
-      if current_stage.name == "Leads"
-        StreakAPI::Box.set_stage(box.key, "Contacted")
+      if current_stage.name == 'Leads'
+        StreakAPI::Box.set_stage(box.key, 'Contacted')
       end
 
-      user_key = Salesperson.where( email: from_email ).any? && Salesperson.find_by( email: from_email ).user_key || nil
-      user_key ||= StreakAPI::User.find_by_email( from_email )
+      user_key = Salesperson.where(email: from_email).any? && Salesperson.find_by(email: from_email).user_key || nil
+      user_key ||= StreakAPI::User.find_by_email(from_email)
 
       if user_key
         StreakAPI::Box.add_follower(@salesperson.streak_api_key, box.key, user_key)
@@ -101,73 +107,78 @@ class PublicController < ApplicationController
 
   private
 
+  def set_request_by_id
+    @request = Request.joins(:user).where(id: params[:request_id] ).first if params[:request_id].present?
+  end
+
   def set_request_by_email
-    requests = Request.joins(:user).where( 'users.email LIKE ?', params[:email] )
-    if requests.any?
-      @request = requests.first
-    else
-      @request = Request.create( user: @user )
-    end
+    return if @request
+    requests = Request.joins(:user).where('users.email LIKE ?', params[:email])
+    @request = requests.any? ? request.first : Request.create( user: @user )
+  end
+
+  def set_shopify_order
+    return unless params[:order_id].present?
+    source_order = ShopifyAPI::Order.find(params[:order_id])
+    @order = Shopify::Order.new source_order
   end
 
   def set_salesperson_by_email
     if params[:sales_email]
       sales_email = params[:sales_email].downcase.strip
 
-      if Salesperson.where( email: sales_email ).any?
-        @salesperson =  Salesperson.find_by_email( sales_email )
+      if Salesperson.where(email: sales_email).any?
+        @salesperson = Salesperson.find_by_email(sales_email)
       else
         password = SecureRandom.hex(8)
-        @salesperson = Salesperson.create( email: sales_email, password: password, password_confirmation: password )
+        @salesperson = Salesperson.create(email: sales_email, password: password, password_confirmation: password)
       end
     end
   end
 
   def set_user_by_email
-    if User.where( email: params[:email] ).any?
-      @user =  User.find_by_email( params[:email] )
+    if User.where(email: params[:email]).any?
+      @user =  User.find_by_email(params[:email])
     else
       password = SecureRandom.hex(8)
-      @user = User.create( email: params[:email], password: password, password_confirmation: password )
+      @user = User.create(email: params[:email], password: password, password_confirmation: password)
     end
   end
 
   def set_user_by_client_id
-    if params[:client_id] && Request.where( client_id: params[:client_id] ).any?
-      user_id = Request.find_by_client_id( params[:client_id] ).user_id
-      @user =  User.find(user_id)
+    if params[:client_id] && Request.where(client_id: params[:client_id]).any?
+      user_id = Request.find_by_client_id(params[:client_id]).user_id
+      @user = User.find(user_id)
     else
       render json: false
     end
   end
 
   def load_products
-    @groups = Shopify::Group.all.reject{ |group|
+    @groups = Shopify::Group.all.reject do |group|
       group.products.count == 0
-    }
+    end
   end
 
   def validate_parameters
-   [:position, :gender, :first_name, :last_name, :client_id ].each do |sym|
-      render( json: false ) if params[sym] == '' or params[sym] == false
+    %i(position gender first_name last_name client_id).each do |sym|
+       render(json: false) if params[sym] ==('') || params[sym] ==(false)
     end
   end
 
   def request_params
-    params.permit( :client_id, :ticket_id, :quote_id, :position, :gender,
-                   :has_color, :is_first_time, :first_name, :last_name, :linker_param, :_ga, :reqid, :salesid )
+    params.permit(:client_id, :ticket_id, :quote_id, :position, :gender,
+                   :has_color, :is_first_time, :first_name, :last_name, :linker_param, :_ga, :reqid, :salesid)
   end
 
   def normalize_email
-    if params[:email]
-      params[:email] = params[:email].downcase.strip
-    end
+    params[:email] = params[:email].downcase.strip if params[:email]
   end
 
   def disable_cache
-    response.headers["Cache-Control"] = "no-cache, no-store"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    response.headers['Cache-Control'] = 'no-cache, no-store'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
   end
 end
 
--- app/models/shopify/order.rb
+++ app/models/shopify/order.rb
@@ -1,38 +1,42 @@
-require 'shopify/base'
+# frozen_string_literal: true
+
+require "shopify/base"
 
 class Shopify::Order < Shopify::Base
-  DEFAULT_SALESPERSON_EMAIL = 'brittany@customtattoodesign.ca'
+  DEFAULT_SALESPERSON_EMAIL = "brittany@customtattoodesign.ca"
 
   def update_request
-    if has_request_id?
-      request = Request.where(id:request_id).first
-    else
-      request = Request.joins(:user).where('users.email LIKE ?', self.customer.email.downcase.strip ).first
-    end
+    request = if has_request_id?
+                Request.where(id: request_id).first
+              else
+                Request.joins(:user).where("users.email LIKE ?", customer.email.downcase.strip).first
+              end
 
-    return unless request
+    return false unless request
 
-    if request.can_convert? and is_deposit?
+    if request.can_convert? && is_deposit?
       request.convert
       request.update_attribute(:deposit_order_id, @source.id)
       request.update_attribute(:state_changed_at, @source.created_at)
-    elsif request.can_complete? and is_final?
+    elsif request.can_complete? && is_final?
       request.complete
       request.update_attribute(:final_order_id, @source.id)
       request.update_attribute(:state_changed_at, @source.created_at)
     end
 
-    if has_salesperson_id? and request.quoted_by_id.nil?
+    if has_salesperson_id? && request.quoted_by_id.nil?
       request.update_attribute :quoted_by_id, salesperson_id
     end
+
+    @source.id == (is_deposit? ? request.deposit_order_id : request.final_order_id)
   end
 
   def request_id
-    note_value('req_id')
+    note_value("req_id")
   end
 
   def salesperson_id
-    note_value('sales_id')
+    note_value("sales_id")
   end
 
   def has_request_id?
@@ -52,97 +56,93 @@ class Shopify::Order < Shopify::Base
   end
 
   def skus
-    @source.line_items.map{|li| li.sku }.uniq
+    @source.line_items.map(&:sku).uniq
   end
 
   def is_deposit?
-    @source.line_items.any? { |line_item| line_item.title.include? 'Deposit' }
+    @source.line_items.any? { |line_item| line_item.title.include? "Deposit" }
   end
 
   def is_final?
-    @source.line_items.any? { |line_item| line_item.title.include? 'Final' }
+    @source.line_items.any? { |line_item| line_item.title.include? "Final" }
   end
 
   def self.find(params)
     digest = Digest::SHA256.base64digest params.inspect
-    Rails.cache.fetch('shopify/orders/' + digest, expires_in: 5.minutes) do
-      ShopifyAPI::Order.all(  params: params ).map{ |c| self.new(c) }
+    Rails.cache.fetch("shopify/orders/" + digest, expires_in: 5.minutes) do
+      ShopifyAPI::Order.all(params: params).map { |c| new(c) }
     end
   end
 
   def self.all(params)
-    self.find_in_batches(params).map{ |order| self.new(order) }
+    find_in_batches(params).map { |order| new(order) }
   end
 
-  def self.count params
+  def self.count(params)
     digest = Digest::SHA256.base64digest params.inspect
-    Rails.cache.fetch('shopify/orders/count/' + digest, expires_in: 5.minutes) do
-      ShopifyAPI::Order.count( params: params )
+    Rails.cache.fetch("shopify/orders/count/" + digest, expires_in: 5.minutes) do
+      ShopifyAPI::Order.count(params: params)
     end
   end
 
-  def self.find_in_batches params
-      order_count    = Shopify::Order.count params
-      nb_pages       = (order_count / params[:limit].to_f).ceil
-      orders         = []
-      1.upto(nb_pages) do |page|
-        params[:page] = page
-        orders = orders + ShopifyAPI::Order.all(params: params )
-      end
-      orders
+  def self.find_in_batches(params)
+    order_count = Shopify::Order.count params
+    nb_pages       = (order_count / params[:limit].to_f).ceil
+    orders         = []
+    1.upto(nb_pages) do |page|
+      params[:page] = page
+      orders += ShopifyAPI::Order.all(params: params)
+    end
+    orders
   end
 
-  def default_salesperson
-
-  end
+  def default_salesperson; end
 
   def note_attribute(name)
     note_attributes.each do |note_attr|
-      if note_attr.name == name
-        return note_attr.value
-      end
+      return note_attr.value if note_attr.name == name
     end
     nil
   end
 
-  MIN_DATE = '2016-06-01T00:00:00-00:00'.to_time
-  def self.attributed params
+  MIN_DATE = "2016-06-01T00:00:00-00:00".to_time
+  def self.attributed(params)
     digest = Digest::SHA256.base64digest params.inspect
-    Rails.cache.fetch('shopify/orders/attributed/' + digest, expires_in: 6.hours) do
+    Rails.cache.fetch("shopify/orders/attributed/" + digest, expires_in: 6.hours) do
       params[:limit] ||= "250"
 
       params[:created_at_min] ||= MIN_DATE
       params[:created_at_max] ||= 1.day.ago.end_of_day
 
-      params[:created_at_min] = [ params[:created_at_min], MIN_DATE.dup ].max
+      params[:created_at_min] = [params[:created_at_min], MIN_DATE.dup].max
       params[:created_at_max] = params[:created_at_max]
 
-      params[:fields] = 'customer,line_items,total_price,subtotal_price,note_attributes,created_at'
+      params[:fields] = "customer,line_items,total_price,subtotal_price,note_attributes,created_at"
 
       orders = Shopify::Order.all(params)
       orders = orders.select do |order|
-        order.line_items.any?{|li| !li.title.include? 'Final' } and
-            ( order.note_attribute('sales_id') or
-                User.where(email: order.customer.email).joins(:requests).
-                where(requests: { created_at: "Wed, 1 Jun 2016".to_date..Time.now } ).any? )
+        order.line_items.any? { |li| !li.title.include? "Final" } &&
+          (order.note_attribute("sales_id") ||
+              User.where(email: order.customer.email).joins(:requests)
+              .where(requests: { created_at: "Wed, 1 Jun 2016".to_date..Time.now }).any?)
       end
 
-      default_salesperson = Salesperson.find_by( email: DEFAULT_SALESPERSON_EMAIL )
+      default_salesperson = Salesperson.find_by(email: DEFAULT_SALESPERSON_EMAIL)
       sale_cutoff = (MIN_DATE.dup - 120.days)
       orders = orders.reject do |order|
-        order.created_at < params[:created_at_min] or order.created_at > params[:created_at_max]
+        order.created_at < (params[:created_at_min]) || order.created_at > (params[:created_at_max])
       end.map do |order|
         order.sales_id = nil
         if order.created_at.to_date < "Tue, 7 Jun 2016".to_date
           order.sales_id = 6
         else
-          noted_id = order.note_attribute('sales_id')
+          noted_id = order.note_attribute("sales_id")
 
           if noted_id
             order.sales_id = noted_id.to_i
           else
-            requests = Request.where(created_at: (sale_cutoff..Time.now) ).where.not(quoted_by_id: nil).
-                joins(:user).where( users: { email: order.customer.email } )
+            requests = Request.where(created_at: (sale_cutoff..Time.now)).where.not(quoted_by_id: nil)
+                              .joins(:user).where(users: { email: order.customer.email })
             order.sales_id = requests.first.quoted_by_id if requests.any?
           end
           order.sales_id ||= default_salesperson.id
@@ -160,11 +160,10 @@ class Shopify::Order < Shopify::Base
 
     @source.note_attributes.each do |note_attr|
       if note_attr.name == attr_name
-        return note_attr.value if note_attr.value != 'undefined'
+        return note_attr.value if note_attr.value != "undefined"
       end
     end
   end
-
 end
 
-require 'digest'
+require "digest"
