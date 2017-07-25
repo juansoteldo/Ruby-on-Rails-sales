5fee2930a6ea25ebdd37dd682c03fe5d12309895
juansoteldo <jsoteldoup@gmail.com>
2017-07-25 08:00:11 -0400

    adds missing streak api key in a streak task

--- app/models/streak_api/box.rb
+++ app/models/streak_api/box.rb
@@ -1,54 +1,53 @@
-require 'streak_api/base'
+# frozen_string_literal: true
+
+require "streak_api/base"
 
 class StreakAPI::Box < StreakAPI::Base
+  def self.all
+    Rails.cache.fetch("streak_box/all", expires_in: 2.minutes) do
+      Streak.api_key = Rails.application.config.streak_api_key
+      Streak::Box.all
+    end
+  end
 
-	def self.all
-		Rails.cache.fetch('streak_box/all', expires_in: 2.minutes) do
-			Streak.api_key = Rails.application.config.streak_api_key
-			Streak::Box.all()
-		end
-	end
+  def self.query(query)
+    Rails.cache.fetch("streak_box/query/" + query, expires_in: 10.seconds) do
+      Streak.api_key = Rails.application.config.streak_api_key
+      results = Streak::Search.query(query).results
+      puts "Ran query with '#{query}', got #{results && results.boxes.count || '0'} boxes"
+      results && results.boxes || [{}]
+    end
+  end
 
-	def self.query(query)
-		Rails.cache.fetch('streak_box/query/'+query, expires_in: 10.seconds) do
-			Streak.api_key = Rails.application.config.streak_api_key
-			results = Streak::Search.query(query).results
-			puts "Ran query with '#{query}', got #{results && results.boxes.count || '0'} boxes"
-			results && results.boxes || [{}]
-		end
-	end
+  def self.find(box_id)
+    Streak.api_key = Rails.application.config.streak_api_key
+    Streak::Box.find(box_id)
+  end
 
-	def self.find(box_id)
+  def self.find_by_email(email)
+    return unless email =~ /\A[^@]+@[^@]+\Z/
 		Streak.api_key = Rails.application.config.streak_api_key
-		Streak::Box.find(box_id)
-	end
+		box_id = StreakAPI::Box.query(email).select do |box|
+      box.name.casecmp(email.downcase).zero?
+    end.last.try(&:box_key)
 
-	def self.find_by_email(email)
-		return unless email =~ /\A[^@]+@[^@]+\Z/
-		box_id = StreakAPI::Box.query(email).select{ |box|
-			box.name.downcase == email.downcase
-		}.last.try(&:box_key)
+    box_id && Streak::Box.find(box_id) || nil
+  end
 
-		box_id && Streak::Box.find(box_id) || nil
-	end
+  def self.set_stage(box_id, stage_name)
+    new_stage = StreakAPI::Stage.find(name: stage_name)
 
-	def self.set_stage(box_id, stage_name)
+    Streak.api_key = Rails.application.config.streak_api_key
+    Streak::Box.update(box_id, stageKey: new_stage.key)
+  end
 
-		new_stage = StreakAPI::Stage.find(name: stage_name)
-
-		Streak.api_key = Rails.application.config.streak_api_key
-		Streak::Box.update(box_id, stageKey: new_stage.key)
-	end
-
-	def self.add_follower(user_api_key, box_id, follower_key)
-		begin
-			# has to be user specific
-			Streak.api_key = user_api_key
-			box = Streak::Box.find(box_id)
-			follower_keys = box.follower_keys | [follower_key]
-			Streak::Box.update(box_id, followerKeys: follower_keys)
-		rescue
-			Rails.logger.warn "Could not add follower with key `#{follower_key}` to box `#{box_id}`"
-		end
-	end
+  def self.add_follower(user_api_key, box_id, follower_key)
+    # has to be user specific
+    Streak.api_key = user_api_key
+    box = Streak::Box.find(box_id)
+    follower_keys = box.follower_keys | [follower_key]
+    Streak::Box.update(box_id, followerKeys: follower_keys)
+  rescue
+    Rails.logger.warn "Could not add follower with key `#{follower_key}` to box `#{box_id}`"
+  end
 end
