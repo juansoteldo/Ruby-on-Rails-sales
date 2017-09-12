c673be083bb97b1579aa6b1d43a6bf386e462e88
juansoteldo <jsoteldoup@gmail.com>
2017-09-12 13:34:14 -0400

    changes email stats to be last few days

--- app/helpers/admin/email_statistics_helper.rb
+++ app/helpers/admin/email_statistics_helper.rb
@@ -6,58 +6,47 @@ module Admin::EmailStatisticsHelper
   def capitalize_and_split(word)
     word.split("_").map(&:capitalize).join(" ")
   end
-  
-  def all_message_of(mailer_type, template = nil)
-    Ahoy::Message.where(utm_campaign: mailer_type, utm_content: template) if template.present?
-    Ahoy::Message.where(utm_campaign: mailer_type)
+
+  def all_message_of(mailer_type, template = nil, range = default_range)
+    messages = ahoy_messages(range).where(utm_campaign: mailer_type)
+    messages = messages.where(utm_content: template) if template.present?
+    messages
   end
 
-  def message_opened(mailer_type, template = nil)
-    if template.present?
-      Ahoy::Message.where(utm_campaign: mailer_type, utm_content: template).where.not(opened_at: nil) 
-    end
-    Ahoy::Message.where(utm_campaign: mailer_type).where.not(opened_at: nil)
+  def opened_messages(mailer_type, template = nil, range = default_range)
+    all_message_of(mailer_type, template, range).where.not(opened_at: nil)
   end
 
-  def message_unopened(mailer_type, template = nil)
-    Ahoy::Message.where(utm_campaign: mailer_type, opened_at: nil, utm_content: template) if template.present?
-    Ahoy::Message.where(utm_campaign: mailer_type, opened_at: nil) 
+  def clicked_messages(mailer_type, template = nil, range = default_range)
+    all_message_of(mailer_type, template, range).where.not(clicked_at: nil)
   end
 
-  def open_percentage_of(mailer_type, template = nil)
-    percent_of(message_opened(mailer_type, template).count ,all_message_of(mailer_type, template).count)
+  def open_percentage_of(mailer_type, template = nil, range = default_range)
+    percent_of(opened_messages(mailer_type, template, range).count , all_message_of(mailer_type, template, range).count)
   end
 
-  def unopened_percentage_of(mailer_type, template = nil)
-    percent_of(message_unopened(mailer_type, template).count ,all_message_of(mailer_type, template).count)
+  def total_email_open_rate(range = default_range)
+    percent_of(ahoy_messages(range).where.not(opened_at: nil).count, ahoy_messages(range).count)
+  end
+
+  def total_click_rate(range = default_range)
+    percent_of(ahoy_messages(range).where.not(clicked_at: nil).count, ahoy_messages(range).count )
+  end
+
+  def click_percentage_of(mailer_type, template = nil, range = default_range)
+    percent_of(clicked_messages(mailer_type, template, range).count ,all_message_of(mailer_type, template, range).count)
+  end
+
+  def ahoy_messages(range = default_range)
+    Ahoy::Message.where(sent_at: range)
+  end
+
+  def default_range
+    [30.days.ago.beginning_of_day..Time.now]
   end
 
   def percent_of(x,n)
-    x.to_f / n.to_f * 100.0
+    return "-" unless n.positive?
+    number_to_percentage(x.to_f / n.to_f * 100, precision: 0)
   end
-
-  def total_email_unopened_rate
-    percent_of(Ahoy::Message.where(opened_at: nil).count, Ahoy::Message.count)
-  end
-
-  def total_email_open_rate
-    percent_of(Ahoy::Message.where.not(opened_at: nil).count, Ahoy::Message.count)
-  end
-
-  def total_click_rate
-     percent_of(Ahoy::Message.where.not(clicked_at: nil).count, Ahoy::Message.count)
-  end
-
-  def message_clicked(mailer_type, template = nil)
-    if template.present?
-      Ahoy::Message.where(utm_campaign: mailer_type, utm_content: template).where.not(clicked_at: nil) 
-    end
-    Ahoy::Message.where(utm_campaign: mailer_type).where.not(clicked_at: nil)
-  end
-
-  def click_percentage_of(mailer_type, template = nil)
-    percent_of(message_clicked(mailer_type, template).count ,all_message_of(mailer_type, template).count)
-  end
-
-
 end
