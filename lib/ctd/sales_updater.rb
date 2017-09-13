b40478980c10d0862ebb4bc546e52f74c3a5ab07
juansoteldo <jsoteldoup@gmail.com>
2017-09-13 09:00:17 -0400

    changes back to using actuals for sales

--- lib/ctd/sales_updater.rb
+++ lib/ctd/sales_updater.rb
@@ -2,30 +2,38 @@ module CTD
   class SalesUpdater
     def self.update
       range = 3.month.ago.beginning_of_month..Time.now.end_of_day
-      epoch_range = range.begin.to_i..range.end.to_i
+
       SalesTotal.where(sold_on: range).delete_all
+      update_sales_totals(range)
+      update_conversion_rates(range)
+    end
+
+    def self.update_sales_totals(range)
       params = {
           created_at_min: range.begin,
           created_at_max: range.end
       }
-      Rails.logger.debug "Loading attributed streak orders for last three months"
+      puts "Loading attributed streak orders for last three months"
       orders = Shopify::Order.attributed params
 
-      Rails.logger.debug "Calculating sales totals for #{orders.count} orders"
+      puts "Calculating sales totals for #{orders.count} orders"
       orders.each do |order|
-        request = Request.find(order.request_id.to_i) if order.request_id.is_a?(String)
-        request ||= Request.joins(:user).where(users: { email: order.customer.email } ).last
+        #request = Request.find(order.request_id.to_i) if order.request_id.is_a?(String)
+        #request ||= Request.joins(:user).where(users: { email: order.customer.email } ).last
 
-        puts "Cannot find request for #{order.customer.email}" unless request
-        created_at = request ? request.created_at.to_date : order.created_at.to_date
+        #        puts "Cannot find request for #{order.customer.email}" unless request
+        created_at = order.created_at.to_date
         total = SalesTotal.where( sold_on: created_at,
                                   salesperson_id: order.sales_id.to_i).first_or_create
         total.order_total += order.total_price.to_f.round(2)
         total.order_count += 1
         total.save!
       end
+    end
 
-      Rails.logger.debug "Updating box counts from last 3 months"
+    def self.update_conversion_rates(date_range)
+      epoch_range = date_range.begin.to_i..date_range.end.to_i
+      puts "Updating box counts from last 3 months"
       done = false
       page = 1
       total_boxes = 0
@@ -50,7 +58,7 @@ module CTD
           salesperson.claim_requests_with_email(customer_email, created_at - 1.day)
 
           sales_total = SalesTotal.where( sold_on: created_at.to_date,
-                                    salesperson_id: salesperson.id).first_or_create
+                                          salesperson_id: salesperson.id).first_or_create
           sales_total.update_attribute :box_count, sales_total.box_count + 1
 
         end
@@ -60,7 +68,6 @@ module CTD
       end
       final_count = Request.where.not(contacted_by_id: nil).count
       puts "Updated salespeople for #{final_count - initial_count} requests based on #{total_boxes} boxes"
-
     end
   end
 end
