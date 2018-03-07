f9217d7cd4cccc9bc7e41b1be1672a5b68376a36
juansoteldo <jsoteldoup@gmail.com>
2018-03-07 11:38:09 -0500

    new 24h marketing email

--- db/seeds/marketing_emails.rb
+++ db/seeds/marketing_emails.rb
@@ -1,25 +1,37 @@
-MarketingEmail.create( state: 'fresh,quoted', days_after_state_change: 1,
-                       from: 'Lee Roller <leeroller@customtattoodesign.ca>',
-                       subject_line: 'Lee Roller Custom Tattoo Design/Owner',
-                       template_name: '24_hour_reminder_email', template_path: 'box_mailer/lead_reminders', version: 1 )
+# frozen_string_literal: true
 
-MarketingEmail.create( state: 'fresh,quoted', days_after_state_change: 7,
-                       from: '"Brittany Greenhill" <brittany@customtattoodesign.ca>',
-                       subject_line: 'New amazing artists for you from Custom Tattoo Design',
-                       template_name: '1_week_reminder_email', template_path: 'box_mailer/lead_reminders', version: 1 )
+MarketingEmail.create(state: "fresh", days_after_state_change: 1,
+                      from: "Lee Roller <leeroller@customtattoodesign.ca>",
+                      subject_line: "Lee Roller Custom Tattoo Design/Owner",
+                      template_name: "24_hour_reminder_email", template_path: "box_mailer/lead_reminders",
+                      version: 1)
 
-MarketingEmail.create( state: 'fresh,quoted', days_after_state_change: 14,
-                       from: 'Brittany Greenhill <brittany@customtattoodesign.ca>',
-                       subject_line: 'Touching Base Brittany Custom Tattoo Design',
-                       template_name: '2_week_reminder_email', template_path: 'box_mailer/lead_reminders', version: 1 )
+MarketingEmail.create(state: "fresh,quoted", days_after_state_change: 7,
+                      from: '"Brittany Greenhill" <brittany@customtattoodesign.ca>',
+                      subject_line: "New amazing artists for you from Custom Tattoo Design",
+                      template_name: "1_week_reminder_email", template_path: "box_mailer/lead_reminders",
+                      version: 1)
 
+MarketingEmail.create(state: "fresh,quoted", days_after_state_change: 14,
+                      from: "Brittany Greenhill <brittany@customtattoodesign.ca>",
+                      subject_line: "Touching Base Brittany Custom Tattoo Design",
+                      template_name: "2_week_reminder_email", template_path: "box_mailer/lead_reminders",
+                      version: 1)
 
-MarketingEmail.create( state: 'deposited,completed', days_after_state_change: 2,
-                       from: 'Kayla McKee <kaylamckee@customtattoodesign.ca>',
-                       subject_line: 'Creating The Best Tattoo with Custom Tattoo Design',
-                       template_name: '48_hour_follow_up_email', template_path: 'box_mailer/customer_follow_ups', version: 1 )
+MarketingEmail.create(state: "deposited,completed", days_after_state_change: 2,
+                      from: "Kayla McKee <kaylamckee@customtattoodesign.ca>",
+                      subject_line: "Creating The Best Tattoo with Custom Tattoo Design",
+                      template_name: "48_hour_follow_up_email", template_path: "box_mailer/customer_follow_ups",
+                      version: 1)
 
-MarketingEmail.create( state: 'deposited,completed', days_after_state_change: 14,
-                       from: 'Kayla McKee <kaylamckee@customtattoodesign.ca>',
-                       subject_line: 'How Is Your Experience with Custom Tattoo Design?',
-                       template_name: '2_week_follow_up_email', template_path: 'box_mailer/customer_follow_ups', version: 1 )
+MarketingEmail.create(state: "deposited,completed", days_after_state_change: 14,
+                      from: "Kayla McKee <kaylamckee@customtattoodesign.ca>",
+                      subject_line: "How Is Your Experience with Custom Tattoo Design?",
+                      template_name: "2_week_follow_up_email", template_path: "box_mailer/customer_follow_ups",
+                      version: 1)
+
+MarketingEmail.create(state: "fresh", days_after_state_change: 1,
+                      from: "Lee Roller <leeroller@customtattoodesign.ca>",
+                      subject_line: "Any questions I can help with?",
+                      template_name: "24_unquoted_email", template_path: "box_mailer/lead_reminders",
+                      version: 1)
