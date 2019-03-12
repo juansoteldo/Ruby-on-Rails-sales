0ae6cfb31cb4516cb7da8e617f413dc3edfb9ae1
juansoteldo <jsoteldoup@gmail.com>
2019-03-12 16:50:20 -0400

    Merging in stashed changes

--- test/controllers/public_controller_test.rb
+++ test/controllers/public_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class PublicControllerTest < ActionController::TestCase
-  test "should get new_request" do
-    get :new_request
+  test "should get new_request with generic request" do
+    get :new_request, { position: "Chest", gender: "Male", first_name: "John", last_name: "Smith", client_id: "123456" }
     assert_response :success
+  end
+
+  test "should fail to get new_request with generic missing parameters" do
+    get :new_request, { position: "Chest", gender: "Male", first_name: "John", client_id: "123456" }
+    assert_response :error
   end
 
 end
