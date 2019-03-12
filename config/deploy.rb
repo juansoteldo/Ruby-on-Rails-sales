8f475d012e301207a591814a3f3ef34ff6f62105
juansoteldo <jsoteldoup@gmail.com>
2019-03-12 17:03:41 -0400

    Hotfix of rbenv version in deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'ssh://git@bitbucket.org/fluxinc/ctd-worklist.git'
 
 set :rbenv_type, :user
 set :rbenv_path, '/home/deploy/.rbenv'
-set :rbenv_ruby, "2.4.4"
+set :rbenv_ruby, "2.5.3"
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
