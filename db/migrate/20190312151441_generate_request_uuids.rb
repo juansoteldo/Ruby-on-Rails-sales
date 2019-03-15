8b3ffb358f055a24ac22a2568e6d3a443b2f1a45
juansoteldo <jsoteldoup@gmail.com>
2019-03-15 00:41:08 -0400

    Does again install pg extensions

--- db/migrate/20190312151441_generate_request_uuids.rb
+++ db/migrate/20190312151441_generate_request_uuids.rb
@@ -1,6 +1,6 @@
 class GenerateRequestUuids < ActiveRecord::Migration[5.2]
   def up
-    # execute "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""
+    execute "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""
     remove_column :requests, :token, :string
     add_column :requests, :uuid, :uuid, index: true, unique: true, default: "uuid_generate_v4()"
   end
