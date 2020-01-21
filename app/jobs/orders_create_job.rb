ba8f457866f74796df6a69bbc29028e2e49b2b22
juansoteldo <jsoteldoup@gmail.com>
2020-01-21 10:12:09 -0500

    Re-adds some things that should not have been removed

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -5,7 +5,9 @@ require "shopify_api"
 class OrdersCreateJob < WebhookJob
   def perform(args)
     super
-    source_order = ShopifyAPI::Order.new(params)
+    source_order = ShopifyAPI::Session.temp(domain: ENV["SHOPIFY_URL"], api_version: "2020-01", token: nil) do
+      ShopifyAPI::Order.new(params)
+    end
     order = MostlyShopify::Order.new source_order
     order.update_request!
     @webhook.commit!(order.request_id)
--- config/initializers/shopify.rb
+++ config/initializers/shopify.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-ShopifyAPI::Base.site = ENV['SHOPIFY_URL']
+ShopifyAPI::Base.site = ENV["SHOPIFY_URL"]
+ShopifyAPI::Base.api_version = "2019-04"
