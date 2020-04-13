2d25a35ae1ba0b606c693bc5e9139ec562538042
juansoteldo <jsoteldoup@gmail.com>
2020-04-13 12:54:22 -0400

    Silenses some noisy errors

--- app/jobs/save_email_job.rb
+++ app/jobs/save_email_job.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require "ctd/errors"
-
 class SaveEmailJob < ApplicationJob
+  StreakBoxNotFoundError = Class.new(StandardError)
   retry_on Streak::APIError, wait: 15.seconds, attempts: 6
-  retry_on CTD::Errors::StreakBoxNotFoundError, wait: 10, attempts: 1
+  retry_on StreakBoxNotFoundError, wait: 10, attempts: 1
 
   TIME_TO_WAIT_FOR_BOX = Rails.env.test? ? 30 : 10
 
   def perform(args)
+    attempt = 0
     @salesperson = args[:salesperson]
     @salesperson.claim_requests_with_email(args[:recipient_email])
     return unless User.find_by_email(args[:recipient_email])
@@ -16,14 +16,23 @@ class SaveEmailJob < ApplicationJob
 
     start = Time.now
     box = nil
-    while Time.now - start < TIME_TO_WAIT_FOR_BOX.seconds
-      box = MostlyStreak::Box.find_by_name(args[:recipient_email])
-      break unless box.nil?
-      sleep 2
+    begin
+      attempt += 1
+      while Time.now - start < TIME_TO_WAIT_FOR_BOX.seconds
+        box = MostlyStreak::Box.find_by_name(args[:recipient_email])
+        break unless box.nil?
+        sleep 2
+      end
+
+      raise StreakBoxNotFoundError, "Cannot find streak box, aborting" if box.nil?
+    rescue StreakBoxNotFoundError
+      if attempt < 4
+        sleep 1  # wait a bit
+        retry
+      else
+        return
+      end
     end
-
-    raise CTD::Errors::StreakBoxNotFoundError, "Cannot find streak box, aborting" if box.nil?
-
     current_stage = box.current_stage
 
     box.set_stage("Contacted") if ["Fresh", "Leads"].include?(current_stage.name)
