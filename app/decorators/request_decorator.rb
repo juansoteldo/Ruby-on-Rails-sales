cb79ef5f599fe8b340f80b4c7c1ae5d467489bc1
juansoteldo <jsoteldoup@gmail.com>
2020-05-10 17:39:23 -0400

    Adds style to emails and forms

--- app/decorators/request_decorator.rb
+++ app/decorators/request_decorator.rb
@@ -4,10 +4,11 @@ class RequestDecorator < Draper::Decorator
   def full_notes
     "Name: #{object.full_name || "Not specified"}
 Email Address: #{object.user.email}
+Position: #{object.position}
+Style: #{object.has_color ? "Yes" : "No"}
+Gender: #{object.gender&.titleize || "Not specified"}
 First Tattoo? #{object.is_first_time ? "Yes" : "No"}
-Gender? #{object.gender&.titleize || "Not specified"}
 In Color? #{object.has_color ? "Yes" : "No"}
-Position? #{object.position}
 
 Description:
 #{object.description}"
