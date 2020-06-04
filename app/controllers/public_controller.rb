0abd1f111c930433ee6837938c9da36273a6442b
juansoteldo <jsoteldoup@gmail.com>
2020-06-04 12:00:49 -0400

    Adds missing attributes

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -164,7 +164,7 @@ class PublicController < ApplicationController
   def request_params
     params.permit(:client_id, :ticket_id, :quote_id, :position, :gender, :first_name, :last_name,
                   :has_color, :is_first_time, :first_name, :last_name, :linker_param, :_ga, :reqid, :salesid, :style,
-                  :description,
+                  :size, :description,
                   user_attributes: [:marketing_opt_in])
   end
 
--- app/views/admin/requests/show.json.jbuilder
+++ app/views/admin/requests/show.json.jbuilder
@@ -1,3 +1,3 @@
 json.extract! @request, :id, :user_id, :uuid, :is_first_time, :gender,
               :has_color, :position, :notes, :quote_id, :client_id,
-              :ticket_id, :created_at, :updated_at
+              :ticket_id, :created_at, :updated_at, :style, :size
