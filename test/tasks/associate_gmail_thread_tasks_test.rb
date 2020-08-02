f07050b0c771028e18e881af6c2acdf05d804072
juansoteldo <jsoteldoup@gmail.com>
2020-08-02 17:50:27 -0400

    style(tests): add comment to gmail thread tasks test

--- test/tasks/associate_gmail_thread_tasks_test.rb
+++ test/tasks/associate_gmail_thread_tasks_test.rb
@@ -56,7 +56,7 @@ class AssociateGmailThreadsTaskTest < ActiveSupport::TestCase
     message = wait_for_and_get_message(request.reload.streak_box_key)
     assert_not_nil message
     assert_emails 1 do
-      perform_enqueued_jobs do
+      perform_enqueued_jobs do # send_quote
         perform_enqueued_jobs do # enqueue_quote_actions
           Rake::Task["recurring:associate_gmail_threads"].invoke
         end
