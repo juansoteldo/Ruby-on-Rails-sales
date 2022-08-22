933c7672b18b0c8ace9577f868f56bf612d4edc2
juansoteldo <jsoteldoup@gmail.com>
2022-08-22 11:30:37 -0400

    feat: add unit testing for transactional emails and newsletter route

--- test/jobs/campaign_monitor_job_test.rb
+++ test/jobs/campaign_monitor_job_test.rb
@@ -11,7 +11,8 @@ class CampaignMonitorJobTest < ActiveJob::TestCase
   end
 
   setup do
-    @user = users(:test)
+    @request = requests(:quoted)
+    @user = @request.user
     # Create a temporary list for testing purposes
     list_title = "temp_list_#{Time.now.to_i}"
     response = CampaignMonitorActionJob.perform_now(method: 'create_list', list_title: list_title)
@@ -38,5 +39,24 @@ class CampaignMonitorJobTest < ActiveJob::TestCase
     response = CampaignMonitorActionJob.perform_now(method: 'delete_subscriber_from_list', list_id: @list_id, user: @user)
     assert response.code == 200
   end
+
+  test 'send transactional email' do
+    smart_email_id = Rails.application.credentials.cm[:transactional_emails][:test]
+    response = CampaignMonitorActionJob.perform_now(method: 'send_transactional_email', smart_email_id: smart_email_id, user: @user)
+    assert response.code == 202
+  end
+
+  test 'newsletter route' do
+    params = {
+      name: @user.first_name,
+      email: @user.email
+    }
+    response = CampaignMonitorActionJob.perform_now(user: { user: params }, method: "add_email_to_marketing_list")
+    assert response.code == 201
+  end
+
+  # TODO:
+  #       - verify custom fields get updated
+  #       - verify remove subscriber
 
 end
