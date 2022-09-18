af1bf688e57906471b31a2c70e522546532592fe
juansoteldo <jsoteldoup@gmail.com>
2022-09-17 21:27:53 -0400

    fix: remove APP_HOST and use APP_URL instead

--- config/initializers/00_app_config.rb
+++ config/initializers/00_app_config.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module CTD
-  APP_HOST = ENV.fetch("APP_HOST", "api.customtattoodesign.ca")
   APP_URL = ENV.fetch("APP_URL", "https://api.customattoodesign.ca")
   CRM_URL = ENV.fetch "CRM_URL", "https://crm.customtattoodesign.ca"
   CHROME_EXTENSION_URL = "https://sdk.customtattoodesign.ca/chrome_extension.zip"
